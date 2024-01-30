d041e4e436c6c322fc7d2a3bdc964ea580789bfe
Ajala John <150349868+Johnnit@users.noreply.github.com>
2024-01-30 23:31:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ var typed = new Typed(".typing", {
 })
 const playVoice = document.querySelector('.play');
 let speechSynthesis = window.speechSynthesis;
-let utter = new SpeechSynthesisUtterance('Welcome, to Ajala John portfolio page.\n Click on the play button on the top-right corner of the page to read the page.');
 
 window.onload = ()=>{
     playVoice.querySelector('i').classList.add('fa-play');
+    let utter = new SpeechSynthesisUtterance('Welcome, to Ajala John portfolio page.\n Click on the play button on the top-right corner of the page to read the page.');
     utter.pitch= 1.5;
-    speechSynthesis.cancel();
+    
     setTimeout(()=>{
         speechSynthesis.speak(utter);
     }, 600);
-    
+    speechSynthesis.cancel();
 }
 
 playVoice.addEventListener('click',()=>{
-    let bdy = document.querySelector('body section').textContent;
+    let bdy = document.querySelector('body').textContent;
     let utterance = new SpeechSynthesisUtterance(bdy);
     if(playVoice.querySelector('i').classList.contains('fa-play')){
         playVoice.querySelector("i").classList.add('fa-pause');
